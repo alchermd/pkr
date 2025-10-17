967923c2fdfe0baf22f9ac5888607d54f500a64b
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-17 22:37:05 +0800

    Display the scenario and answer

--- src/frontend/src/ranges/range-quiz/App.jsx
+++ src/frontend/src/ranges/range-quiz/App.jsx
@@ -1,3 +1,30 @@
-export default function App({ initialData }) {
-  return <h1>Hello, Range Quiz App!</h1>;
+function App({ initialData }) {
+  const { grids, available_positions } = initialData;
+  const scenario = randomScenario(grids, available_positions);
+  return (
+    <p>
+      You were dealt {scenario.dealtCard} from {scenario.position}. What is your
+      action?
+      <br />
+      Answer: {scenario.answer}
+    </p>
+  );
 }
+
+function randomScenario(grids, available_positions) {
+  const randomPosition =
+    available_positions[Math.floor(Math.random() * available_positions.length)];
+  const randomGrid = grids.find(
+    ([gridPosition, gridData]) => gridPosition === randomPosition,
+  )[1];
+  const randomRow = randomGrid[Math.floor(Math.random() * randomGrid.length)];
+  const randomCell = randomRow[Math.floor(Math.random() * randomRow.length)];
+
+  return {
+    position: randomPosition,
+    dealtCard: randomCell.label,
+    answer: randomCell.action,
+  };
+}
+
+export default App;
