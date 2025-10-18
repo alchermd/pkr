4cad65bded508ee493fb4049a81b72e9f1cdbee0
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-18 14:48:57 +0800

    Add cards in the dependency array

--- src/frontend/src/ranges/range-quiz/QuizCards.jsx
+++ src/frontend/src/ranges/range-quiz/QuizCards.jsx
@@ -10,7 +10,7 @@ function QuizCards({ cards }) {
     const suits = generateSuits(cards);
     setCard1Suit(suits[0]);
     setCard2Suit(suits[1]);
-  }, []);
+  }, [cards]);
 
   return (
     <div className="d-flex align-items-center justify-content-center gap-3">
