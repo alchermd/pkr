b95107dbd348a5e6906cb9d3d829aa863dcc2903
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-25 12:23:43 +0800

    Add alias resolution

--- src/frontend/vitest.config.ts
+++ src/frontend/vitest.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   test: {
     include: ["**/*.test.{ts,tsx}"],
     globals: true,
