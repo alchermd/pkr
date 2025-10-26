e97b0a465e60d91c1f314f150ed179437d75e000
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-26 20:13:57 +0800

    Add profile picture tests

---
+++ src/authn/models_tests.py
@@ -0,0 +1,24 @@
+import pytest
+from allauth.socialaccount.models import SocialAccount
+from django.contrib.auth import get_user_model
+
+
+@pytest.fixture
+def User():
+    return get_user_model()
+
+
+@pytest.mark.django_db
+class TestUser:
+    def test_can_resolve_profile_picture_through_google(self, User):
+        user = User.objects.create_user(username="testuser", password="testpass")
+        social_account = SocialAccount.objects.create(
+            user=user,
+            provider="google",
+            extra_data={"picture": "example.com/picture.jpg"},
+        )
+        assert "example.com/picture.jpg" == user.profile_picture
+
+    def test_blank_profile_pictures_for_users_without_google_accounts(self, User):
+        user = User.objects.create_user(username="testuser", password="testpass")
+        assert "" == user.profile_picture
