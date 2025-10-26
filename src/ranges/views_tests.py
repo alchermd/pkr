79a1cf3f9d2b6aaa0325101a0199ba3f6901f0e4
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-26 19:58:54 +0800

    Fix broken range view tests due to auth

--- src/ranges/views_tests.py
+++ src/ranges/views_tests.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth import get_user_model
 from django.test import Client
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
@@ -6,32 +7,42 @@ from pytest_django.asserts import assertTemplateUsed
 from ranges.models import PreFlopRange
 
 
+@pytest.fixture
+def authenticated_client(client):
+    User = get_user_model()
+    test_user = User.objects.create_user(username="testuser", password="testpass")
+    client.login(username="testuser", password="testpass")
+    return client
+
+
 @pytest.mark.django_db
 class TestRangeListView:
-    def test_renders_expected_template(self, client: Client):
-        response = client.get(reverse("ranges:range_list"))
+    def test_renders_expected_template(self, authenticated_client: Client):
+        response = authenticated_client.get(reverse("ranges:range_list"))
         assertTemplateUsed(response, "ranges/range_list.html")
 
 
 @pytest.mark.django_db
 class TestRangeDetailView:
-    def test_renders_expected_template(self, client: Client):
+    def test_renders_expected_template(self, authenticated_client: Client):
         pfr = PreFlopRange.objects.create(name="Test Range", description="A test range")
-        response = client.get(reverse("ranges:range_detail", args=[pfr.id]))
+        response = authenticated_client.get(
+            reverse("ranges:range_detail", args=[pfr.id])
+        )
         assertTemplateUsed(response, "ranges/range_detail.html")
 
-    def test_404_for_invalid_id(self, client: Client):
-        response = client.get(reverse("ranges:range_detail", args=[42]))
+    def test_404_for_invalid_id(self, authenticated_client: Client):
+        response = authenticated_client.get(reverse("ranges:range_detail", args=[42]))
         assert 404 == response.status_code
 
 
 @pytest.mark.django_db
 class TestRangeDetailView:
-    def test_renders_expected_template(self, client: Client):
+    def test_renders_expected_template(self, authenticated_client: Client):
         pfr = PreFlopRange.objects.create(name="Test Range", description="A test range")
-        response = client.get(reverse("ranges:range_quiz", args=[pfr.id]))
+        response = authenticated_client.get(reverse("ranges:range_quiz", args=[pfr.id]))
         assertTemplateUsed(response, "ranges/range_quiz.html")
 
-    def test_404_for_invalid_id(self, client: Client):
-        response = client.get(reverse("ranges:range_quiz", args=[42]))
+    def test_404_for_invalid_id(self, authenticated_client: Client):
+        response = authenticated_client.get(reverse("ranges:range_quiz", args=[42]))
         assert 404 == response.status_code
