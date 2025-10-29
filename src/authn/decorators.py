19024ecd7b3ce1b6062a5d9e234d69d321fca8a5
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-29 14:57:39 +0800

    Add typehints

--- src/authn/decorators.py
+++ src/authn/decorators.py
@@ -1,14 +1,17 @@
 from functools import wraps
-
-from django.conf import settings
+from typing import Any, TypeVar
+from collections.abc import Callable
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
+from django.conf import settings
+
+ViewFunc = TypeVar("ViewFunc", bound=Callable[..., HttpResponse])
 
 
-def guest_required(function):
-    def decorator(view_func):
+def guest_required(function: ViewFunc | None = None) -> Callable[[ViewFunc], ViewFunc]:
+    def decorator(view_func: ViewFunc) -> ViewFunc:
         @wraps(view_func)
-        def f(request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        def f(request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
             if request.user.is_authenticated:
                 return redirect(settings.LOGIN_REDIRECT_URL)
             return view_func(request, *args, **kwargs)
@@ -17,5 +20,4 @@ def guest_required(function):
 
     if function is not None:
         return decorator(function)
-
     return decorator
