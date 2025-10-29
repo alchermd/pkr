2acc99edb9f60d8032af66987f956fdc7e3e8343
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-29 14:49:57 +0800

    Remove unused variable

--- src/conftest.py
+++ src/conftest.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 @pytest.fixture
 def authenticated_client(client, User):
-    test_user = User.objects.create_user(username="testuser", password="testpass")
+    User.objects.create_user(username="testuser", password="testpass")
     client.login(username="testuser", password="testpass")
     return client
 
