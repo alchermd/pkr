588d32baab5077b0f81cf310ae0069f6229abd46
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-29 14:50:27 +0800

    Prepend unused variable with _

--- src/config/context_processors.py
+++ src/config/context_processors.py
@@ -3,5 +3,5 @@ from django.conf import settings
 from django.http import HttpRequest
 
 
-def debug_flag(request: HttpRequest) -> dict[str, bool]:
+def debug_flag(_request: HttpRequest) -> dict[str, bool]:
     return {"DEBUG": settings.DEBUG}
