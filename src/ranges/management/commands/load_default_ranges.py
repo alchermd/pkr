8ac0d00e9c285e233a8c6c05e762f7f62c7a3b57
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-10-29 14:25:15 +0800

    Prepend unused parameters with _, use Path instead of os.path

--- src/ranges/management/commands/load_default_ranges.py
+++ src/ranges/management/commands/load_default_ranges.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 from django.core.management import BaseCommand
 
@@ -9,21 +9,20 @@ from ranges.models import PreFlopRange
 class Command(BaseCommand):
     help = "Load default preflop ranges into the database if they do not exist"
 
-    def handle(self, *args, **options):
+    def handle(self, *_args, **_options) -> None:
         deleted, _ = PreFlopRange.objects.filter(is_default=True).delete()
         self.stdout.write(
             self.style.WARNING(f"Deleted {deleted} existing default ranges.")
         )
 
         loaded_ranges_count = 0
-        for filename in os.listdir(RANGE_FILES_DIR):
-            if filename.endswith(".csv"):
-                base_name = os.path.splitext(filename)[0]
-                domain_preflop_range = load_range(base_name)
-                preflop_range = PreFlopRange.objects.from_domain(domain_preflop_range)
-                preflop_range.is_default = True
-                preflop_range.save()
-                loaded_ranges_count += 1
+        for path in Path(RANGE_FILES_DIR).glob("*.csv"):
+            base_name = path.stem
+            domain_preflop_range = load_range(base_name)
+            preflop_range = PreFlopRange.objects.from_domain(domain_preflop_range)
+            preflop_range.is_default = True
+            preflop_range.save()
+            loaded_ranges_count += 1
 
         self.stdout.write(
             self.style.SUCCESS(f"Loaded {loaded_ranges_count} preflop ranges.")
